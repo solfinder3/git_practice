afd490a8ec7c5966df2c3b64de67b188e08e579e
Skyler <skylerpeacock@yahoo.com>
2020-08-26 19:00:45 -0600

    create file

---
+++ notes.rb
@@ -0,0 +1,37 @@
+
+GIT
+  - version control system
+  - keep track of your coding version 
+  - 2005 LinusTorvalds
+  - store, collabs 
+Github
+  - all versions of your code 
+  - bitbuckets 
+  - hosting service. 
+  - Ruby on Rails 
+  - 2008 
+  - 2018 ms 7.5 Billion 
+    - 7.5 Billion minute  14,269 years 
+    - $100,000 / year 75,000 years 
+    - Canada $208.33
+brew install git 
+ssh 
+
+
+
+Repositories
+- all projects will be a repository
+Warning* 
+- system directory as a repo (Desktop, Docs, Application)
+- never create a repo in another repo 
+- delete .git 
+
+repository are useally personal data dont do warning*
+
+
+ls -a =see hidden files
+  rm -rf .git to delete file
+
+
+  git (commands)
+  
